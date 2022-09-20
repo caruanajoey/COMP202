572867719e335028c44e5822026c7ef4373fef44
1gaojer <113914089+1gaojer@users.noreply.github.com>
2022-09-19 22:04:07 -0400

    Update triangles.py

--- triangles.py
+++ triangles.py
@@ -17,33 +17,33 @@ def is_special_triangle():
         print("The triangle is equilateral")
     if (side_1==side_2) or (side_2==side_3) or (side_1==side_3):
         print ("The triangle is isosceles")
-    is_side1_hypotenuse = side_1==math.sqrt(side_2**2+side_3**2)
-    is_side2_hypotenuse = side_2==math.sqrt(side_1**2+side_3**2)
-    is_side3_hypotenuse = side_3==math.sqrt(side_2**2+side_1**2)
+    is_side1_hypotenuse = side_1 == math.sqrt(side_2**2+side_3**2)
+    is_side2_hypotenuse = side_2 == math.sqrt(side_1**2+side_3**2)
+    is_side3_hypotenuse = side_3 == math.sqrt(side_2**2+side_1**2)
     if is_side1_hypotenuse or is_side2_hypotenuse or is_side3_hypotenuse:
         print ("The triangle is right-angle")
         
 def perimeter_of_triangle():
-    a= float(input("side a: "))
-    b= float(input("side b: "))
-    c= float(input("side c: "))
+    a = float(input("side a: "))
+    b = float(input("side b: "))
+    c = float(input("side c: "))
     print(a+b+c)
     
 def area_of_triangle():
-    a= float(input("side a: "))
-    b= float(input("side b: "))
-    c= float(input("side c: "))
+    a = float(input("side a: "))
+    b = float(input("side b: "))
+    c = float(input("side c: "))
     per = float((a + b + c)/2)
-    area = round((math.sqrt((per * (per-a) * (per-b) * (per-c)))),2)
+    area = (math.sqrt((per * (per-a) * (per-b) * (per-c))))
     print(area)
     
 def angles_of_triangle():
     a = float(input ("Enter side 1 length: "))
     b = float(input ("Enter side 2 length: "))
     c = float(input ("Enter side 3 length: "))
-    alpha=math.acos((math.pow(b,2)+math.pow(c,2)-math.pow(a,2))/(2*b*c))
-    beta=math.acos((math.pow(a,2)+math.pow(c,2)-math.pow(b,2))/(2*a*c))
-    gamma= math.acos((math.pow(a,2)+math.pow(b,2)-math.pow(c,2))/(2*a*b))
+    alpha = math.acos((math.pow(b,2)+math.pow(c,2)-math.pow(a,2))/(2*b*c))
+    beta = math.acos((math.pow(a,2)+math.pow(c,2)-math.pow(b,2))/(2*a*c))
+    gamma = math.acos((math.pow(a,2)+math.pow(b,2)-math.pow(c,2))/(2*a*b))
     print(alpha*180/math.pi)
     print(beta*180/math.pi)
     print(gamma*180/math.pi)
