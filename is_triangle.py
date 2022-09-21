bba5e407a01ae810f29d99496c91dfd8d9da81a1
zacharyqh <113945190+zacharyqh@users.noreply.github.com>
2022-09-20 23:17:58 -0400

    Update is_triangle.py

--- is_triangle.py
+++ is_triangle.py
@@ -4,7 +4,7 @@ def is_triangle():
     a = float (input ("Enter side a: "))
     b = float (input ("Enter side b: "))
     c = float (input ("Enter side c: "))
-    if ((a + b) > c ) and ((a + c) > b) and ((b + c) > a):
+    if ((a + b) >= c ) and ((a + c) >= b) and ((b + c) >= a):
         print ("Yes, there is a triangle with side lengths a, b and c!")
     else:
         print ("No, there isn't a triangle with side lengths a, b and c!") 
