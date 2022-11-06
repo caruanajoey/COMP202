0fb005de3a3b8b3f3c3359d8a69537d041d36001
Philippe Aprahamian <91009265+philippeapra@users.noreply.github.com>
2022-11-06 11:36:06 -0500

    Create challenge.py

---
+++ challenge.py
@@ -0,0 +1,24 @@
+def make_constellation_in_sky(sky):
+    start=""
+    for row in range(len(sky)):
+        for column in range (len(sky[row])):
+            if sky[row][column] == "*" and start !="":
+                sky[row][start+1:column]=generate_list(column-start-1,'-')
+                
+                start=column
+            elif sky[row][column] == "*":
+                start = column
+        start =""
+    
+    for column in range(len(sky[row])):
+        for row in range (len(sky)):
+            if sky[row][column] == "*" and start !="":
+                for q in range (start+1,row):
+                    sky[q][column] = "|"
+                start=row
+            elif sky[row][column] == "*":
+                start = row
+        start =""
+    
+    return sky
+  
